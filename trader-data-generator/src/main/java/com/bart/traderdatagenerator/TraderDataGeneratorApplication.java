b60d054719206daa69e74d87c38e2e55448fd818
Bartmax112 <bartmax112@gmail.com>
2024-04-16 16:19:18 +0200

    Init

---
+++ trader-data-generator/src/main/java/com/bart/traderdatagenerator/TraderDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.bart.traderdatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderDataGeneratorApplication.class, args);
+	}
+
+}
