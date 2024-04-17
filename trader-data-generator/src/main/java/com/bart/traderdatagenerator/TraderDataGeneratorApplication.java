45a55ce162c653d014ff0abfb81e7a8f47ebffa2
Bartmax112 <bartmax112@gmail.com>
2024-04-17 17:39:29 +0200

    Connected producer to kafka cluster

--- trader-data-generator/src/main/java/com/bart/traderdatagenerator/TraderDataGeneratorApplication.java
+++ trader-data-generator/src/main/java/com/bart/traderdatagenerator/TraderDataGeneratorApplication.java
@@ -1,13 +1,31 @@
 package com.bart.traderdatagenerator;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TraderDataGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TraderDataGeneratorApplication.class, args);
+		Properties properties = new Properties();
+		properties.setProperty("bootstrap.servers","kafka-1:9092");
+		properties.setProperty("key.serializer",StringSerializer.class.getName());
+
+		properties.setProperty("value.serializer",StringSerializer.class.getName());
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+		
+		ProducerRecord<String, String> record = new ProducerRecord<String,String>("first_topic", "Second message in docker container!");
+
+		producer.send(record);
+
+		producer.flush();
+
+		producer.close();
 	}
 
 }
