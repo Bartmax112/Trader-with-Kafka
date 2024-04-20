8e6ed0dca2f528bacabaa8dfa37e5216d7804a37
Bartmax112 <bartmax112@gmail.com>
2024-04-20 20:57:51 +0200

    Added sketch of navigation bar

--- vaadin-fe/src/main/java/org/vaadin/bart/charts/ChartView.java
+++ vaadin-fe/src/main/java/org/vaadin/bart/charts/ChartView.java
@@ -3,11 +3,16 @@ package org.vaadin.bart.charts;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.router.Route;
+
+import org.vaadin.bart.GreetService;
+
 import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.ListSeries;
 import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLink;
+import org.vaadin.bart.MainView;
 
 
 @Route
@@ -15,6 +20,17 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class ChartView extends VerticalLayout {
     
     public ChartView() {
+
+        RouterLink homeLink = new RouterLink("Hello", MainView.class);
+        // RouterLink aboutLink = new RouterLink("About", AboutView.class);
+
+        // Optionally, you can style the menu items
+        homeLink.addClassName("menu-item");
+        // aboutLink.addClassName("menu-item");
+
+        add(homeLink);
+
+
         Chart chart = new Chart();
         Configuration configuration = chart.getConfiguration();
         configuration.setTitle("Sample Line Chart");
