be83f2c12cbefcfdbb800a3d15a1e632592d33d1
Bartmax112 <bartmax112@gmail.com>
2024-04-28 12:08:59 +0200

    Serialized data sent and received with kafka

--- trader-data-generator/src/main/java/com/bart/traderdatagenerator/TraderDataGeneratorApplication.java
+++ trader-data-generator/src/main/java/com/bart/traderdatagenerator/TraderDataGeneratorApplication.java
@@ -1,11 +1,14 @@
 package com.bart.traderdatagenerator;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @SpringBootApplication
 public class TraderDataGeneratorApplication {
@@ -13,13 +16,15 @@ public class TraderDataGeneratorApplication {
 	public static void main(String[] args) {
 		Properties properties = new Properties();
 		properties.setProperty("bootstrap.servers","kafka-1:9092");
-		properties.setProperty("key.serializer",StringSerializer.class.getName());
+		properties.setProperty("key.serializer", StringSerializer.class.getName());
 
-		properties.setProperty("value.serializer",StringSerializer.class.getName());
+		properties.setProperty("value.serializer",JsonSerializer.class.getName());
 
-		KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+		KafkaProducer<String, DataStructure> producer = new KafkaProducer<>(properties);
+
+		var valueToTransfer = new DataStructure(Date.from(Instant.now()), "100");
 		
-		ProducerRecord<String, String> record = new ProducerRecord<String,String>("first_topic", "Second message in docker container!");
+		ProducerRecord<String, DataStructure> record = new ProducerRecord<>("gold", valueToTransfer);
 
 		producer.send(record);
 
